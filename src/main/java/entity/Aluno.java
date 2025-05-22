7e4c6afb474c06437177a837d866f718590eef57
briwno <brunoalvesbezerra.ba@gmail.com>
2025-05-22 14:01:47 -0300

    init

--- src/main/java/entity/Aluno.java
+++ src/main/java/entity/Aluno.java
@@ -1,13 +1,44 @@
 package entity;
 
+import java.util.List;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TB_Aluno")
 public class Aluno {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_aluno")
+    private Long id;
+
+    @Column(name = "nome", length = 100, nullable = false)
     private String nome;
+
+    @Column(name = "cpf", length = 11, nullable = false, unique = true)
     private String cpf;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+        name = "TB_Aluno_Curso",
+        joinColumns = @JoinColumn(name = "id_aluno"),
+        inverseJoinColumns = @JoinColumn(name = "id_curso")
+    )
+    private List<Curso> cursos;
+
+    public Aluno() {}
 
     public Aluno(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -21,17 +52,25 @@ public class Aluno {
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
     }
 
     @Override
     public String toString() {
         return "Aluno{" +
-                "nome='" + nome + '\'' +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
                 ", cpf='" + cpf + '\'' +
                 '}';
     }
-
-
 }
--- src/main/java/entity/Curso.java
+++ src/main/java/entity/Curso.java
@@ -1,4 +1,51 @@
 package entity;
 
-public class Curso {
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_Curso")
+public class Curso{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_curso")
+    private Long id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @ManyToMany(mappedBy = "cursos", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Aluno> alunos = new ArrayList<>();
+
+    public Curso() {}
+
+    public Curso(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
 }
